5414d3269e9a1b8998979f8548089c689dea3987
Christian Dürnberger <chris_d85@hotmail.com>
2017-02-25 17:06:54 +0100

    0.0.2

---
+++ test/index.js
@@ -0,0 +1,47 @@
+
+
+// http://10.0.0.4:8080/raumserver/data/getZoneConfig
+var raumserverHost = "http://10.0.0.4:8080";
+var raumserverRequestData = raumserverHost + "/raumserver/data/";
+var raumserverRequestController = raumserverHost + "/raumserver/controller/";
+
+
+function doGetAction(_action)
+{
+    $.ajax({
+                url: raumserverRequestData + _action,
+                cache: false,
+                async: false,
+                success: function(res, status, xhr) 
+                {
+                    showJSONResult( res );
+                },
+                error: function(xhr, textStatus, errorThrown)
+                {
+                    showJSONResult({ "error" : errorThrown.message });
+                }
+            });    
+}
+
+function doAction(_action)
+{
+    $.ajax({
+                url: raumserverRequestController + _action,
+                cache: false,
+                async: false,
+                success: function(res, status, xhr) 
+                {
+                    showJSONResult( res );
+                },
+                error: function(xhr, textStatus, errorThrown)
+                {
+                    showJSONResult({ "error" : errorThrown.message });
+                }
+            });    
+}
+
+
+function showJSONResult(_result)
+{
+    $("#jsonResult").html(JSON.stringify(_result));
+}
