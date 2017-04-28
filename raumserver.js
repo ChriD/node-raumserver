7bf6a27de5abeebeb20b9db9f285ca00ae626fc3
Christian Dürnberger <chris_d85@hotmail.com>
2017-04-28 20:22:58 +0200

    #6

---
+++ raumserver.js
@@ -0,0 +1,39 @@
+'use strict'; 
+var Config = require('config');
+var Raumserver = require('./lib/lib.raumserver');
+
+var raumserver = new Raumserver();
+
+setConfiguration("raumserver", "port");
+setConfiguration("raumfeld", "raumfeldHostRequestPort");
+setConfiguration("raumfeld", "raumfeldManufacturerId");
+setConfiguration("raumfeld", "raumfeldVirtualMediaPlayerModelDescription");
+setConfiguration("raumfeld", "alivePingerIntervall");
+setConfiguration("raumfeld", "ssdpDiscovertimeout");
+setConfiguration("raumfeld", "bonjourDiscoverTimeout");
+setConfiguration("raumfeld", "uriMetaDataTemplateFile");
+setConfiguration("raumfeld", "rendererStateTriggerConfirmationTimout");
+
+raumserver.createLogger(4);
+raumserver.init();
+
+
+function setConfiguration(_object, _key) {
+    try
+    {
+        if(_object == "raumserver")
+            raumserver.settings[_key] = Config.get(_object + '.' + _key); 
+        else
+            raumserver.raumkernel.settings[_key] = Config.get(_object + '.' + _key);   
+    }
+    catch(_exception)
+    {
+        raumserver.logError("Configuration key '" + _object + "." + _key + "' not set! Using default!", _exception);
+    } 
+}
+
+function execute() {
+}
+
+setInterval(execute,1000);
+
